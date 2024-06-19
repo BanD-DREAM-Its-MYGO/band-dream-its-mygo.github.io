24019c2038b7000b39b5da0a62cd6171c99f2736
BanD-DREAM-Its-MYGO <ck11200874@gl.ck.tp.edu.tw>
2024-06-19 17:33:28 +0800

    Update style.js

--- style.js
+++ style.js
@@ -1,4 +1,15 @@
-$("h2").hover(function(){
-  $(this).css("background-color", "#cccccc");
+
+$(document).ready(function(){
+
+$("h2").hover(
+  function(){
+  $(this).css("background-color", "#000000");
   $(this).css("color", "tomato");
+},
+function(){
+  $(this).css("background-color", "#ffffff");
+  $(this).css("color", "black");
+}
+);
+
 });
